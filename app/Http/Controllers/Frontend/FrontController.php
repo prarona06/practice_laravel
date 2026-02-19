8f6e396ce71f69e273e9f2f94d410733b30a8e4b
Prarona Dutta <prarona.dutta.06@gmail.com>
2026-02-19 14:35:43 +0600

    laravel log

--- app/Http/Controllers/Frontend/FrontController.php
+++ app/Http/Controllers/Frontend/FrontController.php
@@ -6,13 +6,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Controllers\IndexController;
 use App\Http\Requests\storePostRequest;
+use Illuminate\Support\Facades\Log;
 
 class FrontController extends Controller
 {
 public function store(storePostRequest $request)
 {
-$validatedData = $request->validated();
-dd($validatedData);
-
+    Log::warning('warning massage');
+Log::info('store method call with dada:', $request->all());
 }
 }
