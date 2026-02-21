dd285ad54db5ed6cc7949a355e945a224189abfe
Prarona Dutta <prarona.dutta.06@gmail.com>
2026-02-22 00:33:52 +0600

    seed factory

--- database/factories/ShopFactory.php
+++ database/factories/ShopFactory.php
@@ -15,9 +15,14 @@ class ShopFactory extends Factory
      * @return array<string, mixed>
      */
     public function definition(): array
-    {
-        return [
-            //
+    {       return [
+            'shop_name' => $this->faker->company(),
+            'shop_number' => $this->faker->unique()->numberBetween(1, 100),
+            'shop_address' => $this->faker->address(),
+            'shop_phone' => $this->faker->unique()->numerify('555#######'),
+            'shop_email' => $this->faker->unique()->safeEmail(),
+            'shop_tin_number' => 'TIN' . $this->faker->unique()->numerify('######'),
         ];
+
     }
 }
--- database/seeders/ShopSeeder.php
+++ database/seeders/ShopSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+App\Models\Shop;
+
 
 class ShopSeeder extends Seeder
 {
@@ -13,17 +15,7 @@ class ShopSeeder extends Seeder
      */
  public function run(): void
 {
-    for ($i = 1; $i <= 10; $i++) {
-        DB::table('shops')->insert([
-            'shop_name' => 'My Shop ' . $i,
-            'shop_number' => $i,
-            'shop_address' => '123 Main St ' . $i,
-            'shop_phone' => 5551234 + $i,
-            'shop_email' => 'myshop' . $i . '@example.com',
-            'shop_tin_number' => 'TIN123456',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+   App\Models\Shop::factory()->count(10)->create();
     }
 }
-}
+
