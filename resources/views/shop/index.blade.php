d40ed79ee0011ed3b1366fd05a23e5cc8d22f137
Prarona Dutta <prarona.dutta.06@gmail.com>
2026-02-24 17:18:09 +0600

    add table

---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,34 @@
+@extends('welcome ')
+
+@section('content')
+
+<div class="container mt-5">
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">srial</th>
+      <th scope="col">shop name</th>
+      <th scope="col">shop number</th>
+      <th scope="col">shop phone</th>
+       <th scope="col">shop email</th>
+        <th scope="col">tin numbr</th>
+         <th scope="col">Ation</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+        <td>@mdo</td>
+          <td>@mdo</td>
+            <td
+        >
+        <a href="#" class="btn btn-primary">Edit</a>
+        <a href="#" class="btn btn-danger">Delete</a>
+        </td>
+    </tr>
+  
+
+@endsection
