0b34b5dc3867be088351a2d429a7f18913cefa62
Prarona Dutta <prarona.dutta.06@gmail.com>
2026-03-04 16:52:51 +0600

    customar index

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Customer;
 
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $customers =Customer::all();
+        dd($customers);
+    }
+
 }
---
+++ resources/views/customer/index.blade.php
@@ -0,0 +1,64 @@
+@extends('welcome')
+
+@section('content')
+
+<div class="container mt-5">
+       <!--display success message-->
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+         @endif
+         <h4>Total Shops: {{ $ShopCount }}</h4>
+    <h4 class="text-center">Shop List</h4>
+    <a href="{{ route('shop.create') }}" class="btn btn-success mb-3">Add New Shop</a>
+    <form action="{{ route('shop.index') }}" method="GET" class="mb-3">
+        <div class="input-group">
+            <input type="text" name="search" class="form-control" placeholder="Search shops..." value="{{ request('search') }}">
+            <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="submit">Search</button>
+            </div>
+        </div>
+
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">srial</th>
+      <th scope="col">shop name</th>
+      <th scope="col">shop number</th>
+      <th scope="col">shop phone</th>
+       <th scope="col">shop email</th>
+        <th scope="col">tin numbr</th>
+         <th scope="col">Ation</th>
+    </tr>
+  </thead>
+ <tbody>
+@forelse ($ShopLists as $Shop)
+<tr>
+    <th scope="row">{{$Shop->id}}</th>
+    <td>{{$Shop->shop_name}}</td>
+    <td>{{$Shop->shop_number}}</td>
+    <td>{{$Shop->shop_phone}}</td>
+    <td>{{$Shop->shop_email}}</td>
+    <td>{{$Shop->shop_tin_number}}</td>
+    <td>
+        <a href="{{ route('shop.edit', $Shop->id) }}" class="btn btn-primary">Edit</a>
+        <form action="{{ route('shop.delete', $Shop->id) }}" method="POST" style="display:inline;">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this shop?')">Delete</button>
+        </form>
+    </td>
+</tr>
+@empty
+<tr></tr>
+    <td colspan="7" class="text-center">No shops found.</td>
+</tr>
+@endforelse
+
+</tbody>
+</table>
+{{ $ShopLists->firstItem() }} - {{ $ShopLists->lastItem() }} of {{ $ShopLists->total() }}
+{{ $ShopLists->Links() }}
+</div>
+@endsection
