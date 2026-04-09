b825f2f942878842406ec154660e002b079854ad
Prarona Dutta <prarona.dutta.06@gmail.com>
2026-04-09 21:56:48 +0600

    creat customer_details table

---
+++ database/migrations/2026_04_09_155054_create_customers_details_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers_details', function (Blueprint $table) {
+            $table->id();
+
+            // foreign key (one-to-one)
+            $table->unsignedBigInteger('customer_id')->unique();
+
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+
+            $table->timestamps();
+
+            // relation with customers table
+            $table->foreign('customer_id')
+                  ->references('id')
+                  ->on('customers')
+                  ->onDelete('cascade');
+        });
+
+
+
+      
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers_details');
+    }
+};
